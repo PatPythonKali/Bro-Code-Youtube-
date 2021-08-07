0d633a907be2a948897e265f9cbebe779886af28
ubuntu <patpyhtonkali@gmail.com>
2021-08-07 17:54:18 +0800

    Python multiple assignment

---
+++ Python Course/Python multiple assignment 🔠.py
@@ -0,0 +1,27 @@
+# Multiple assignment = allows us to assign multiple variables at the same
+# in one line of code
+
+# # Regular Way
+# name = "Bro"
+# age = 21
+# attractive = True
+#
+# # Shortcut
+# name, age, attractive = "Bro", 21, True
+
+# print(name)
+# print(age)
+# print(attractive)
+
+# # Regular Way
+# Spongebob = 30
+# Patrick = 30
+# Sandy = 30
+# Squidward = 30
+
+Spongebob = Patrick = Sandy = Squidward = 30
+
+print(Spongebob)
+print(Patrick)
+print(Sandy)
+print(Squidward)

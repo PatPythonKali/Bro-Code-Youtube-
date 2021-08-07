0e9b26da662dca3a8b54e31f2ceeada1c4502dc5
ubuntu <patpyhtonkali@gmail.com>
2021-08-07 22:29:22 +0800

    Python lists 🧾

---
+++ Python Course/Python lists 🧾.py
@@ -0,0 +1,15 @@
+# list = used to tore multiple items in a single variable
+
+food = ["pizza", "humburger", "hotdog", "spaghetti", "pudding"]
+food[0] = "sushi"
+# print(food[0])
+
+# food.append("ice cream")
+# food.remove("hotdog")
+# food.pop()
+# food.insert(0, "cake")
+# food.sort()
+# food.clear()
+
+for x in food:
+    print(x)

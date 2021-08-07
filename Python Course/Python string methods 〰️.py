66a8d4438f1a780c89058eea59b6ac85e2b78757
ubuntu <patpyhtonkali@gmail.com>
2021-08-07 18:07:53 +0800

    Python string methods 〰️

---
+++ Python Course/Python string methods 〰️.py
@@ -0,0 +1,11 @@
+name = "bro code"
+
+print(len(name))
+print(name.find("r"))
+print(name.capitalize())
+print(name.lower())
+print(name.isdigit())
+print(name.isalpha())
+print(name.count("o"))
+print(name.replace("o", "a"))
+print(name * 3)

66d4c381e194c9a04d2a4c9a082538902e84ad00
ubuntu <patpyhtonkali@gmail.com>
2021-08-07 23:41:41 +0800

    Python sets 🍴

---
+++ Python Course/Python sets 🍴.py
@@ -0,0 +1,16 @@
+# set = collection to unordered, unindexed. no duplicate values
+
+utensils = {"fork", "spoon", "knife"}
+dishes = {"bowl", "plate", "cup", "knife"}
+
+# utensils.add("napkin")
+# utensils.remove("fork")
+# utensils.clear()
+# dishes.update(utensils)
+# dinner_table = utensils.union(dishes)
+
+# print(utensils.difference(dishes))
+print(utensils.intersection(dishes))
+
+# for x in dinner_table:
+#     print(x)

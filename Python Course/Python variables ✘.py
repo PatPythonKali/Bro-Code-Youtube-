bbee4f17bed61241ffccad15fdf5640142974a35
Pat M <PatPythonKali>
2021-08-07 14:24:14 +0800

    Python variables ✘

---
+++ Python Course/Python variables ✘.py
@@ -0,0 +1,31 @@
+# variable = a container for a value. Behaves as the value that it contains.
+name = "Bro"
+print("Hello " + name)
+
+# To check datatype
+print(type(name))
+
+
+first_name = "Bro"
+last_name = "Code"
+full_name = first_name + " " + last_name
+print(full_name)
+
+age = 21
+print(type(age))
+
+# Regular Way of typing age
+age = age + 1
+# Shortcut
+age += 1
+print(age)
+
+# print("Your age is: " + str(age))
+
+height = 250.5
+print(type(height))
+print("Your height is:" + str(height))
+
+human = False
+print(type(human))
+print("Are you a human: " + str(human))
